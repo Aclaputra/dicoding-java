6290030bc3d5d8c91d8fb1919529b6816b7e3667
Muhammad Acla <muhamadacla22@gmail.com>
2022-03-27 05:00:29 +0700

    modified src/com/dicoding/javafundamental/accessmodifier/package4/Test.java

--- src/com/dicoding/javafundamental/accessmodifier/package4/Test.java
+++ src/com/dicoding/javafundamental/accessmodifier/package4/Test.java
@@ -12,9 +12,13 @@ class Test implements Serializable {
 	
 	/**
 	 * Synchronized modifier digunakan untuk membatasi akses ke-
-	 * suatu variable/methods yang hanya boleh dilakukan oleh satu thread
+	 * suatu variable/methods yang hanya boleh dilakukan oleh satu threa.
+	 * 
+	 * Ketika ada 2 thread yang ingin mengakses synchronized variable/methods,
+	 * maka prosesnya akan dilakukan secara serial (bergantian)
 	 */
+	
 	public synchronized void showData() {
-		
+		//contoh
 	}
 }
